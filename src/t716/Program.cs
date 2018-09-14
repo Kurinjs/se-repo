067aebc4fde76e12fa923ffc6b5cf22af4db7f0b
DESKTOP-7IM6F89\Артур <kurinjs@mail.ru>
2018-09-15 00:12:21 +0300

    Sufranovich sorry amlet

---
+++ src/t716/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+using System.Windows;
+using System.Drawing;
+
+
+namespace TRPOtask2
+{
+    class GetProcessesByNameClass
+    {
+        static public System.Timers.Timer tm = new System.Timers.Timer();
+        static public System.Timers.Timer tm1 = new System.Timers.Timer();
+        static void Main(string[] args)
+        {
+            tm.Elapsed += new System.Timers.ElapsedEventHandler(FirstVariant);
+            tm.Interval = 500;
+            tm1.Elapsed += new System.Timers.ElapsedEventHandler(SecondVariant);
+            tm1.Interval = 500;
+            Console.Write("Choose your variant: (1/2) ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    {
+                        tm.Enabled = true;
+                        break;
+                    }
+                case 2:
+                    {
+                        tm1.Enabled = true;
+                        break;
+                    }
+                default:
+                    {
+                        Environment.Exit(0);
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+        public static void SecondVariant(object send, ElapsedEventArgs a)
+        {
+            Console.Clear();
+            PerformanceCounter ob1 = new PerformanceCounter("Процессор", "% загруженности процессора", "_Total");
+            PerformanceCounter ob2 = new PerformanceCounter("Память", "% использования выделенной памяти");
+            Console.WriteLine("Процессор загружен на {0}% , Память загружена на {1}%  ", ob1.NextValue(), ob2.NextValue());
+            Console.Write("[");
+            for (int i = 0; i < 100; i++)
+            {
+                if (i < (int)ob2.NextValue())
+                    Console.Write("|");
+                else
+                    Console.Write(".");
+            }
+            Console.Write("]\n\n");
+        }
+        public static void FirstVariant(object send, ElapsedEventArgs a)
+        {
+            Console.Clear();
+            Thread.Sleep(1000);
+            int i = 0; double some1 = 0; double some2 = 0;
+            Process[] processes = Process.GetProcesses();
+            Console.WriteLine("{0,-25} ||| {1,-11} ||| {2,-11}||| {3,-11} |||", "Name", "PID", "RAM_Ussage", "CPU_ussage");
+            Console.WriteLine("\n\n\n");
+
+            foreach (Process p in processes)
+            {
+                some2 += p.PagedSystemMemorySize64 / 1000000;
+                i++;
+                some1 += p.WorkingSet64 / 1000000;
+                Console.WriteLine("{0,-25}|||{1,-11}|||{2,-11}|||{3,-11}|||", p.ProcessName, p.Id, p.PagedSystemMemorySize64 / 1000000, p.PagedMemorySize64 / 1000000);
+            }
+            Console.WriteLine("\n\n"
+                + "\n" +
+                some1
+                + " Resultate:\n Count process: "
+                + i +
+                "\nCount public CPU: "
+                + some2
+                + "\nMachineName: "
+                + Environment.MachineName);
+
+        }
+    }
+}
